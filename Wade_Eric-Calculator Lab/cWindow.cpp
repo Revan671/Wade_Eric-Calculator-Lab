4b0f3cf97f40e3860fdad594383aea831b6b5432
Eric Wade <ECWade1@student.fullsail.edu>
2022-06-13 19:15:50 -0600

    Added button prototypes to cWindow.h

--- Wade_Eric-Calculator Lab/cWindow.cpp
+++ Wade_Eric-Calculator Lab/cWindow.cpp
@@ -2,7 +2,7 @@
 
 cWindow::cWindow() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(400,200), wxSize(500,500))
 {
-
+	
 }
 
 cWindow::~cWindow() {
--- Wade_Eric-Calculator Lab/cWindow.h
+++ Wade_Eric-Calculator Lab/cWindow.h
@@ -5,5 +5,33 @@ class cWindow : public wxFrame
 public:
 	cWindow();
 	~cWindow();
+public:
+	//numpad
+	wxButton* button0;
+	wxButton* button1;
+	wxButton* button2;
+	wxButton* button3;
+	wxButton* button4;
+	wxButton* button5;
+	wxButton* button6;
+	wxButton* button7;
+	wxButton* button8;
+	wxButton* button9;
+	//controls
+	wxButton* button10; // +
+	wxButton* button11; // -
+	wxButton* button12; // x
+	wxButton* button13; // /
+	wxButton* button14; // =
+	wxButton* button15; // c
+	wxButton* button16; // mod
+	wxButton* button17; // binary
+	wxButton* button18; // hex
+	wxButton* button19; // decimal
+	wxButton* button20; // +/-
+	//IO
+	wxTextCtrl* outputBox;
+	wxTextCtrl* inputBox;
+
 };
 
