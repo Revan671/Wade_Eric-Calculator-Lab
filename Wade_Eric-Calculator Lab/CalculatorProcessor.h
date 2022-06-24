45e60d54f6a3d49d4af8995ffa19713c9dd8ca4b
Eric Wade <ECWade1@student.fullsail.edu>
2022-06-24 16:21:45 -0600

    commented out GetDivision from Calculator Processor

--- Wade_Eric-Calculator Lab/CalculatorProcessor.h
+++ Wade_Eric-Calculator Lab/CalculatorProcessor.h
@@ -24,7 +24,7 @@ public:
 	//std::string GetAddition(int addNum);
 	//std::string GetSubtraction(int subNum);
 	//std::string GetMultiplication(int multNum);
-	std::string GetDivision(int divNum);
+	//std::string GetDivision(int divNum);
 	std::string GetModulus(int modNum);
 
 };
