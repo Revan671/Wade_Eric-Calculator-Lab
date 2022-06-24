d9c4ba2d6d32eff46e270e41d6053f2090c2233e
Eric Wade <ECWade1@student.fullsail.edu>
2022-06-24 14:03:20 -0600

    Cleaned up code, removed commented out old implementaion

--- Wade_Eric-Calculator Lab/cWindow.cpp
+++ Wade_Eric-Calculator Lab/cWindow.cpp
@@ -11,25 +11,15 @@ cWindow::cWindow() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(400,200), 
 {
 	ButtonFactory* Factory = new ButtonFactory();
 	//numpad
-	//button7 = new wxButton(this, 107, "7", wxPoint(10, 130), wxSize(70, 50));
 	button7 = Factory->CreateNumpadButton(7, this, 10, 130);
-	//button8 = new wxButton(this, 108, "8", wxPoint(80, 130), wxSize(70, 50));
 	button8 = Factory->CreateNumpadButton(8, this, 80, 130);
-	//button9 = new wxButton(this, 109, "9", wxPoint(150, 130), wxSize(70, 50));
 	button9 = Factory->CreateNumpadButton(9, this, 150, 130);
-	//button4 = new wxButton(this, 104, "4", wxPoint(10, 180), wxSize(70, 50));
 	button4 = Factory->CreateNumpadButton(4, this, 10, 180);
-	//button5 = new wxButton(this, 105, "5", wxPoint(80, 180), wxSize(70, 50));
 	button5 = Factory->CreateNumpadButton(5, this, 80, 180);
-	//button6 = new wxButton(this, 106, "6", wxPoint(150, 180), wxSize(70, 50));
 	button6 = Factory->CreateNumpadButton(6, this, 150, 180);
-	//button1 = new wxButton(this, 107, "1", wxPoint(10, 230), wxSize(70, 50));
 	button1 = Factory->CreateNumpadButton(1, this, 10, 230);
-	//button2 = new wxButton(this, 108, "2", wxPoint(80, 230), wxSize(70, 50));
 	button2 = Factory->CreateNumpadButton(2, this, 80, 230);
-	//button3 = new wxButton(this, 109, "3", wxPoint(150, 230), wxSize(70, 50));
 	button3 = Factory->CreateNumpadButton(3, this, 150, 230);
-	//button0 = new wxButton(this, 100, "0", wxPoint(80, 280), wxSize(70, 50));
 	button0 = Factory->CreateNumpadButton(0, this, 80, 280);
 	//controls
 	button20 = Factory->CreateSignButton(this);
